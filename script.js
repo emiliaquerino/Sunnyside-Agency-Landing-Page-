bfb83e9688090fc3c146967a4f674d28eb5c1707
Emília Querino <emiliaquerino@gmail.com>
2024-01-24 16:10:43 -0300

    Cria a função js para rolar a página até o seção do link

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', scrollToSection);
+    });
+
+    function scrollToSection(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+});
