48062c87f09ecadd6a71a118fa1b57236111fdf2
Emília Querino <emiliaquerino@gmail.com>
2024-01-27 09:02:49 -0300

    importa o javascript

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const nav = document.querySelector(".navbar-nav");
-const navToggleBtn = document.querySelector(".nav-toggle-btn");
-const navLinks = document.querySelectorAll(".link-nav");
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".navbar-nav");
+  const navToggleBtn = document.querySelector(".nav-toggle-btn");
+  const navLinks = document.querySelectorAll(".link-nav");
 
-const navToggleFunc = function () {
-  nav.classList.toggle('active');
-  navToggleBtn.classList.toggle('active');
-}
+  const navToggleFunc = function () {
+    nav.classList.toggle("active");
+    navToggleBtn.classList.toggle("active");
+  };
 
-navToggleBtn.addEventListener('click', navToggleFunc);
+  navToggleBtn.addEventListener("click", navToggleFunc);
 
-for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener('click', function () {
-    navToggleFunc();
-  });
-}
-
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+      navToggleFunc();
+    });
+  }
+});
